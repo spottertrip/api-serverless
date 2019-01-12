30af9f89bf3f5014f3ccbbb437aa32afd761cebc
Masselot Antoine <antoine.masselot.pro@gmail.com>
2019-01-12 14:51:48 +0100

    [setup/database] Create dynamo db tables definitions (#25)

--- src/datastore/index.ts
+++ src/datastore/index.ts
@@ -1,8 +1,8 @@
 import { DataStore } from '@datastore/datastore'
-import aws, { AWSOptions } from '@datastore/drivers/aws'
+import AWSDriver, { AWSOptions } from '@datastore/drivers/aws'
 
 export let datastore: DataStore
 export const initializeDB = (options: AWSOptions) => {
   // Initialize Database Connection
-  datastore = new aws(options)
+  datastore = new AWSDriver(options)
 }

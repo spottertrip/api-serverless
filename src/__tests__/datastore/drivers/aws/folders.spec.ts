6a2232887194077420600598005e4a459b4b12cc
Antoine Masselot <amasselot@hotmail.com>
2019-01-28 11:52:12 +0100

    [UTests/Datastore/Folders] Add unit tests for listing folders datastore

--- src/__tests__/datastore/drivers/aws/folders.spec.ts
+++ src/__tests__/datastore/drivers/aws/folders.spec.ts
@@ -1,13 +1,13 @@
 jest.mock('aws-sdk')
 
 import InternalServerError from '@errors/InternalServerError'
-import { listSpotters } from '@datastore/drivers/aws/spotters'
 import * as AWSMock from 'aws-sdk-mock'
 import * as AWS from 'aws-sdk'
 import { v4 } from 'uuid'
-import { createFolder } from '@datastore/drivers/aws/folders'
+import { createFolder, listFolders } from '@datastore/drivers/aws/folders'
 import { IFolder } from '@models/Folder';
-import TravelBand from '../../../../models/TravelBand'
+import TravelBand from '@models/TravelBand'
+import NotFoundError from '@errors/NotFoundError';
 
 AWSMock.setSDKInstance(AWS)
 
@@ -44,4 +44,30 @@ test('create folder properly', async () => {
   expect(folder.name).toBe(fixtureFolder.name)
   expect(folder.description).toBe(fixtureFolder.description)
   AWSMock.restore('DynamoDB.DocumentClient')
+})
+
+// ---- LIST FOLDER ---- //
+
+test('list travel band folders with not existing ID -> not found', async () => {
+  const mockedGet = jest.fn((params: any, cb: any) => {
+    return cb(null, { Item: null })
+  })
+  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
+  await expect(listFolders(new AWS.DynamoDB.DocumentClient(), v4())).rejects.toThrow(NotFoundError)
+  AWSMock.restore('DynamoDB.DocumentClient')
+})
+
+test('list travel band folders with valid ID', async () => {
+  const testFolder = {
+    name: 'testing folder',
+    description: 'description folder',
+    folderId: v4(),
+  }
+  const mockedGet = jest.fn((params: any, cb: any) => {
+    return cb(null, { Item: { folders: [testFolder] } })
+  })
+  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
+  const folders = await listFolders(new AWS.DynamoDB.DocumentClient(), v4())
+  expect(folders.length).toBe(1)
+  expect(folders[0].folderId).toBe(testFolder.folderId)
 })

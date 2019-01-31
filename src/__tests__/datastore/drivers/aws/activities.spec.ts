5ece554631ef843c2690c8e76c68d819bbe62094
Antoine Masselot <amasselot@hotmail.com>
2019-01-31 10:25:49 +0100

    [Tests/Datastore/Bookings] Update unit tests for list bookings

--- src/__tests__/datastore/drivers/aws/activities.spec.ts
+++ src/__tests__/datastore/drivers/aws/activities.spec.ts
@@ -5,13 +5,12 @@ import {
   getActivity,
   listActivities,
   listTravelBandActivities,
-  listTravelBandBookings, shareActivity,
+  shareActivity,
 } from '@datastore/drivers/aws/activities'
 import InternalServerError from '@errors/InternalServerError'
 import * as AWSMock from 'aws-sdk-mock'
 import * as AWS from 'aws-sdk'
 import { v4 } from 'uuid'
-import BadRequestError from '@errors/BadRequestError'
 import NotFoundError from '@errors/NotFoundError'
 
 AWSMock.setSDKInstance(AWS)
@@ -35,33 +34,6 @@ test('list travel band activities with valid ID', async () => {
   AWSMock.mock('DynamoDB.DocumentClient', 'query', mockedQuery)
   const activities = await listTravelBandActivities(new AWS.DynamoDB.DocumentClient(), v4())
   expect(activities.length).toBe(2)
-  AWSMock.restore('DynamoDB.DocumentClient')
-})
-
-// ---- List travel band Bookings ---- //
-
-test('list travel band bookings without ID returns bad request', async () => {
-  await expect(listTravelBandBookings(new AWS.DynamoDB.DocumentClient(), '')).rejects.toThrow(BadRequestError)
-})
-
-test('list travel band bookings with not existing ID -> not found', async () => {
-  const mockedGet = jest.fn((params: any, cb: any) => {
-    return cb(null, { Item: null })
-  })
-  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
-  await expect(listTravelBandBookings(new AWS.DynamoDB.DocumentClient(), v4())).rejects.toThrow(NotFoundError)
-  AWSMock.restore('DynamoDB.DocumentClient')
-})
-
-test('list travel band bookings with valid ID', async () => {
-  const activityName = 'test Activity'
-  const mockedGet = jest.fn((params: any, cb: any) => {
-    return cb(null, { Item: { bookings: [{ name: activityName }] } })
-  })
-  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
-  const activities = await listTravelBandBookings(new AWS.DynamoDB.DocumentClient(), v4())
-  expect(activities.length).toBe(1)
-  expect(activities[0].name).toBe(activityName)
   AWSMock.restore('DynamoDB.DocumentClient')
 })
 

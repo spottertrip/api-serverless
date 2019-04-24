703788cfae98c45c8b79a28a8fa9bc06502a6bdd
Antoine Masselot <amasselot@hotmail.com>
2019-04-24 14:25:22 +0200

    [Unit/Bookings/List] Add Unit tests for handler/datastore - list bookings and get travel bands for spotter

--- src/__tests__/datastore/drivers/aws/bookings.spec.ts
+++ src/__tests__/datastore/drivers/aws/bookings.spec.ts
@@ -1,8 +1,10 @@
 import * as AWSMock from 'aws-sdk-mock'
 import * as AWS from 'aws-sdk'
 import { v4 } from 'uuid'
-import { listBookingsForTravelBand } from '@datastore/drivers/aws/bookings'
+import { listBookingsForTravelBand, listAllBookings } from '@datastore/drivers/aws/bookings'
 import InternalServerError from '@errors/InternalServerError'
+import NotFoundError from '@errors/NotFoundError';
+import DatabaseError from '@errors/DatabaseError';
 
 AWSMock.setSDKInstance(AWS)
 
@@ -35,5 +37,47 @@ test('list travel band bookings with valid ID', async () => {
   const gotBookings = await listBookingsForTravelBand(new AWS.DynamoDB.DocumentClient(), travelBandId)
   expect(gotBookings.length).toBe(1)
   expect(gotBookings[0].bookingId).toBe(bookingId)
+  AWSMock.restore('DynamoDB.DocumentClient')
+})
+
+// --- List Bookings for a spotter
+
+test('get bookigns for a spotter with not existing ID -> not found', async () => {
+  const mockedGet = jest.fn((params: any, cb: any) => {
+    return cb(null, { Item: null })
+  })
+  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
+  await expect(listAllBookings(new AWS.DynamoDB.DocumentClient(), v4())).rejects.toThrow(NotFoundError)
+  AWSMock.restore('DynamoDB.DocumentClient')
+})
+
+test('get bookings for a spotter - database error', async () => {
+  const mockedGet = jest.fn((params: any, cb: any) => {
+    throw new Error('db error')
+  })
+  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
+  await expect(listAllBookings(new AWS.DynamoDB.DocumentClient(), v4())).rejects.toThrow(DatabaseError)
+  AWSMock.restore('DynamoDB.DocumentClient')
+})
+
+test('get travel band ids for spotters with valid ID', async () => {
+  const spotterId = v4()
+  const mockedGet = jest.fn((params: any, cb: any) => {
+    return cb(null, { Item: { travelBands: [v4(), v4(), v4()] } })
+  })
+  const mockedBookings = jest.fn((params: any, cb: any) => {
+    return cb(null, { Items: [
+      {
+        bookingId: v4(),
+      },
+      {
+        bookingId: v4(),
+      },
+    ]})
+  })
+  AWSMock.mock('DynamoDB.DocumentClient', 'get', mockedGet)
+  AWSMock.mock('DynamoDB.DocumentClient', 'scan', mockedBookings)
+  const bookings = await listAllBookings(new AWS.DynamoDB.DocumentClient(), spotterId)
+  expect(bookings.length).toBe(2)
   AWSMock.restore('DynamoDB.DocumentClient')
 })

21db0c6c0a867740b7d3681137e3d704e7e4d23b
Antoine Masselot <amasselot@hotmail.com>
2019-04-24 14:25:43 +0200

    [Handler/Bookings] Add handler list all bookings for a spotter

---
+++ src/handlers/bookings/listAllBookings.ts
@@ -0,0 +1,22 @@
+import { datastore } from '@datastore/index';
+import { handleError } from '@helpers/http';
+import { IBooking } from '@models/Booking';
+
+export const listAllBookings = async (event, context) => {
+  console.log('toto')
+  let bookings: IBooking[] = []
+  // TODO: remove hard coded user ID when Authentication is ready.
+  const userId = '15a992e1-8d3f-421e-99a3-2ba5d2131d82'
+
+  // Fetch Bookings
+  try {
+    bookings = await datastore.listAllBookings(userId);
+  } catch (error) {
+    return handleError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ bookings }),
+  }
+}
